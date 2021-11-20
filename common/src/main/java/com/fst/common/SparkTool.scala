fe7ddfcb72d48f5fa53d6cd677061059a916d61f
184535658@qq.com <1845356586@qq.com>
2021-11-20 11:02:45 +0800

    相关工具类

---
+++ common/src/main/java/com/fst/common/SparkTool.scala
@@ -0,0 +1,37 @@
+package com.fst.common
+
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.SparkSession
+
+abstract class SparkTool extends Logging {
+  var day_id: String = _
+
+  def main(args: Array[String]): Unit = {
+
+    /**
+     * 获取时间参数
+     */
+    if (args.length == 0) {
+      // 使用Logging中打印日志的方法
+      logError("请传入时间参数")
+      return
+    }
+
+    day_id = args(0)  // 需要我们自己传进来
+    logInfo(s"时间参数:$day_id")
+
+    logInfo("创建Spark环境")
+    /**
+     * 构建spark环境
+     */
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName(this.getClass.getSimpleName.replace("$", "")) //类名拿到的时候会多一个$,因此要替换掉
+      .enableHiveSupport() // 开启Hive支持
+      .getOrCreate()
+    //调用子类的run方法
+    run(spark)
+  }
+
+  def run(spark: SparkSession)
+}
